baf65d7f74fa7f1716c3b61bc8f840f4a97b5075
rohit21755 <rohit.k09022002@gmail.com>
2025-01-19 22:02:51 +0530

    first commit

---
+++ src/routes/user.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+const router = express.Router();
+
+router.post('/signup', (req,res)=>{
+    
+})
+
+router.post('/sigin', (req,res)=>{
+
+})
